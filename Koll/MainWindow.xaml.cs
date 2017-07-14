8e199e2bd4c02cce4694cb2ab2f14e101a363202
Jumar Macato <hikari.netto23@gmail.com>
2017-07-14 15:23:23 +0800

    - Initial Commit -
    Implement Desktop Background hooking courtesy of ( https://www.codeproject.com/articles/856020/draw-behind-desktop-icons-in-windows ).

--- Koll/MainWindow.xaml.cs
+++ Koll/MainWindow.xaml.cs
@@ -8,10 +8,12 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Koll
 {
@@ -23,6 +25,74 @@ namespace Koll
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Fetch the Progman window
+            IntPtr progman = W32.FindWindow("Progman", null);
+
+            IntPtr result = IntPtr.Zero;
+
+            // Send 0x052C to Progman. This message directs Progman to spawn a 
+            // WorkerW behind the desktop icons. If it is already there, nothing 
+            // happens.
+            W32.SendMessageTimeout(progman,
+                                   0x052C,
+                                   new IntPtr(0),
+                                   IntPtr.Zero,
+                                   W32.SendMessageTimeoutFlags.SMTO_NORMAL,
+                                   1000,
+                                   out result);
+
+            IntPtr workerw = IntPtr.Zero;
+
+            W32.EnumWindows(new W32.EnumWindowsProc((tophandle, topparamhandle) =>
+            {
+                IntPtr p = W32.FindWindowEx(tophandle,
+                                            IntPtr.Zero,
+                                            "SHELLDLL_DefView",
+                                            IntPtr.Zero);
+
+                if (p != IntPtr.Zero)
+                {
+                    // Gets the WorkerW Window after the current one.
+                    workerw = W32.FindWindowEx(IntPtr.Zero,
+                                               tophandle,
+                                               "WorkerW",
+                                               IntPtr.Zero);
+                }
+
+                return true;
+            }), IntPtr.Zero);
+
+            IntPtr windowHandle = new WindowInteropHelper(this).Handle;
+
+            W32.SetParent(windowHandle, workerw);
+
+            this.Width = SystemParameters.PrimaryScreenWidth;
+            this.Height = SystemParameters.PrimaryScreenHeight;
+            WindowState = WindowState.Maximized;
+            WindowStyle = WindowStyle.None;
+
+
+            DispatcherTimer ClockTimer = new DispatcherTimer();
+            ClockTimer.Tick += ClockTimer_Tick;
+            ClockTimer.Interval = TimeSpan.FromMilliseconds(50);
+            ClockTimer.Start();
+
+        }
+
+        string OldTime = "";
+
+        private void ClockTimer_Tick(object sender, EventArgs e)
+        {
+            var newTime = DateTime.Now.ToString("hh\u2236mm");
+            if (OldTime != newTime)
+            {
+                this.Dispatcher.Invoke(() => TimeLabel.Content = newTime);
+                OldTime = newTime;
+            }
         }
     }
 }
